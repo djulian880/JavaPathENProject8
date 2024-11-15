f9553e774e51e56672edae5b5c54dc9e11b11985
Julien GUTTER <julien.gutter@gmail.com>
2024-11-15 10:03:22 +0200

    Maj test en concordance avec type renvoyé par fonction getNearByAttractions

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.UUID;
 
+import com.openclassrooms.tourguide.DTO.FiveNearestAttractionsDTO;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -92,7 +93,7 @@ public class TestTourGuideService {
 		assertEquals(user.getUserId(), visitedLocation.userId);
 	}
 
-	@Disabled // Not yet implemented
+	//@Disabled // Not yet implemented
 	@Test
 	public void getNearbyAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
@@ -103,11 +104,10 @@ public class TestTourGuideService {
 		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
 		VisitedLocation visitedLocation = tourGuideService.trackUserLocation(user);
 
-		List<Attraction> attractions = tourGuideService.getNearByAttractions(visitedLocation);
-
+		FiveNearestAttractionsDTO fiveNearestAttractionsDTO= tourGuideService.getNearByAttractions(visitedLocation,user);
 		tourGuideService.tracker.stopTracking();
 
-		assertEquals(5, attractions.size());
+		assertEquals(5, fiveNearestAttractionsDTO.nearbyAttractions.size());
 	}
 
 	public void getTripDeals() {
